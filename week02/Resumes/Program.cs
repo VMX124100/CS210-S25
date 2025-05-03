6b9478dac1f0472f350d550f19f494c6fbf7a4de
David martinez <davidmartinezburgos2004@gmail.com>
2025-05-02 23:01:13 -0600

    finished

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job("Software Engineer", "Lex Corp", 2015, 2020);
+        Job job2 = new Job("Senior Developer", "Web Os Company", 2020, 2023);
+        Job job3 = new Job("Project Manager (Hostage)", "Global Tech North Korea", 2023, 2999);
+
+        Resume my_resume = new Resume();
+        my_resume._name = "David Martinez";
+        my_resume._jobs.Add(job1);
+        my_resume._jobs.Add(job2);
+        my_resume._jobs.Add(job3);
+        
+        my_resume.DisplayResume();
     }
 }
