839557606a331c645a24fae45e09374d679487f9
David martinez <davidmartinezburgos2004@gmail.com>
2025-05-19 18:13:37 -0600

    incompleted

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualBasic;
+
+public class Scripture
+{
+    public Reference _reference;
+    private List<string> _words = new List<string>();
+    public Scripture(Reference reference)
+    {
+        _reference = reference;
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+    
+    }
+
+    public GetDisplayText()
+    {
+    
+    }
+
+    public bool IscompletedHiden();
+    {
+        
+    }
+
+}   
