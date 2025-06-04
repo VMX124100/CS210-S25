3b2cdaf603cabdcacbd99437d5bcece231902365
David martinez <davidmartinezburgos2004@gmail.com>
2025-06-04 17:19:28 -0600

    Shapes Done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square(3,"Red");
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle(4, 5, "Blue");
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle(6, "Green");
+        shapes.Add(shape3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
